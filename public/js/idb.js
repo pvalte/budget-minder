8cd1d09109c5a88fe4050574c31a56ed50601c51
Paige A <pvaltemare@gmail.com>
2021-12-23 00:07:34 -0500

    fix upload records

--- public/js/idb.js
+++ public/js/idb.js
@@ -11,7 +11,7 @@ request.onsuccess = function (event) {
     db = event.target.result;
 
     if (navigator.onLine) {
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
 
@@ -32,7 +32,7 @@ function uploadTransaction() {
 
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-            fetch('/api/transactions', {
+            fetch("/api/transaction", {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
@@ -57,3 +57,5 @@ function uploadTransaction() {
         }
     };
 }
+
+window.addEventListener('online', uploadTransaction);
